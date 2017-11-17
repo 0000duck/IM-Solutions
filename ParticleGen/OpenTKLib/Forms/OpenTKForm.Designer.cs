25e17dd2bfa5f2365e84e46283ea975bc697275f
HurricanKai <herrucan.schmitt@gmail.com>
2017-11-17 20:00:46 +0100

    update icon

--- ParticleGen/OpenTKLib/Forms/OpenTKForm.Designer.cs
+++ ParticleGen/OpenTKLib/Forms/OpenTKForm.Designer.cs
@@ -30,31 +30,29 @@ namespace OpenTKLib
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OpenTKForm));
-            this.panelOpenTK = new System.Windows.Forms.Panel();
-            this.SuspendLayout();
-            // 
-            // panelOpenTK
-            // 
-            this.panelOpenTK.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panelOpenTK.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelOpenTK.Location = new System.Drawing.Point(0, 0);
-            this.panelOpenTK.Name = "panelOpenTK";
-            this.panelOpenTK.Size = new System.Drawing.Size(1312, 651);
-            this.panelOpenTK.TabIndex = 1;
-            // 
-            // OpenTKForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1312, 651);
-            this.Controls.Add(this.panelOpenTK);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.KeyPreview = true;
-            this.Name = "OpenTKForm";
-            this.Text = "OpenTK Form";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OpenTKForm_FormClosed);
-            this.ResumeLayout(false);
+			this.panelOpenTK = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// panelOpenTK
+			// 
+			this.panelOpenTK.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.panelOpenTK.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelOpenTK.Location = new System.Drawing.Point(0, 0);
+			this.panelOpenTK.Name = "panelOpenTK";
+			this.panelOpenTK.Size = new System.Drawing.Size(1312, 651);
+			this.panelOpenTK.TabIndex = 1;
+			// 
+			// OpenTKForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1312, 651);
+			this.Controls.Add(this.panelOpenTK);
+			this.KeyPreview = true;
+			this.Name = "OpenTKForm";
+			this.Text = "OpenTK Form";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OpenTKForm_FormClosed);
+			this.ResumeLayout(false);
 
         }
 
